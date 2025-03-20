40e03f8a6a66a8ea8915f14e6db2ed18b6253ccc
Nancy Novotna <d23974@oa-opava.cz>
2025-03-20 08:12:46 +0100

    Nova funkce: umocneni
    #10

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from utils import secti
+from utils import secti, umocnit
 
 def main():
     x = 5
     y = 10
     vysledek = secti(x, y)
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
+    print(f"Výsledek umocnění čísla {x} je: {umocnit(x)}")
+
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -21,3 +21,6 @@ def odecti(a, b):
 
 def deleni(a,b):
     return a / b
+
+def umocnit(a):
+    return a ** 2
