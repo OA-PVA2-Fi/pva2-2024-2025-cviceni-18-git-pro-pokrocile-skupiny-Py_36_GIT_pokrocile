b28fc7f09adc40944a37a5fec34cbe2b7aa0da7d
Daniel Slunsky <d23371@oa-opava.cz>
2025-03-20 08:12:47 +0100

    Nová funkce: Obsah trojuhelniku #8

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from utils import secti
+from utils import secti, obsah_trojuhelniku
+
 
 def main():
     x = 5
     y = 10
     vysledek = secti(x, y)
     print(f"Výsledek sčítání {x} a {y} je: {vysledek}")
+    obsah_trojuhelniku(1,x,y)
+    print(f"Obsah trojuhelniku {x} a {y} je: {vysledek}")
 
 if __name__ == '__main__':
     main()
